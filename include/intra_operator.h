93df04e600386290db02bcbb35a9d3d92b161d6c
Anurag <Anurag.Ramaswamy@iiitb.ac.in>
2024-11-15 12:12:05 +0530

    Added intra_operator files

---
+++ include/intra_operator.h
@@ -0,0 +1,22 @@
+#include "problem.h"
+#include "solution.h"
+#include "route_context.h"//
+#include <vector>
+
+class IntraOperator {
+public:
+  virtual ~IntraOperator() = default;
+  virtual bool operator()(const Problem &problem, Node route_index, SpecificSolution &solution,
+                          RouteContext &context) const = 0;
+};
+
+class Exchange : public IntraOperator {
+public:
+  bool operator()(const Problem &problem, Node route_index, SpecificSolution &solution,
+                  RouteContext &context) const override;
+};
+template <int num> class OrOpt : public IntraOperator {
+public:
+  bool operator()(const Problem &problem, Node route_index, SpecificSolution &solution,
+                  RouteContext &context) const override;
+};
