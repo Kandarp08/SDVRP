94a3a21701e33c02ebf5d9913cc877844a1d3a2f
Ananthakrishna <Ananthakrishna.K@iiitb.ac.in>
2024-11-26 18:52:31 +0530

    added config in param list

--- include/solver.h
+++ include/solver.h
@@ -1,18 +1,20 @@
 #ifndef SOLVER_H
 #define SOLVER_H
 
+
 #include "problem.h"
+#include "config.h"
 #include "solution.h"
 
 
-template <typename SolutionType, typename ProblemType> class Solver {
+template <typename SolutionType, typename ConfigType, typename ProblemType> class Solver {
     public:
-        virtual SolutionType Solve(const ProblemType &problem) = 0;
+        virtual SolutionType Solve(const ConfigType &config, const ProblemType &problem) = 0;
 };
 
-class SpecificSolver : public Solver<SpecificSolution, Problem> {
+class SpecificSolver : public Solver<SpecificSolution, SpecificConfig, Problem> {
     public:
-        SpecificSolution Solve(const Problem &problem) override;
+        SpecificSolution Solve( const SpecificConfig &config,const Problem &problem) override;
 };
 
 
