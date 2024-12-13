fe48c8db8e58e902c6efc7f5c6436436042ae63a
Anurag <Anurag.Ramaswamy@iiitb.ac.in>
2024-12-13 11:02:27 +0530

    Added comments for intra-operator

--- src/intra_operator/exchange.cpp
+++ src/intra_operator/exchange.cpp
@@ -2,32 +2,56 @@
 #include "../../include/delta.h"
 #include "../../include/route_context.h"
 
-
+// Structure representing an exchange move between two nodes in a route
 struct ExchangeMove {
   Node node_a;
   Node node_b;
 };
 
+// Performs the actual node exchange in a given route
+// Parameters:
+// - move: The ExchangeMove containing nodes to be swapped
+// - route_index: Index of the route where exchange occurs
+// - solution: Current solution being modified
+// - context: Route context tracking route-specific information
 void DoExchange(const ExchangeMove &move, Node route_index, SpecificSolution &solution, RouteContext &context) {
+  // Find predecessors and successors of nodes to be exchanged
   Node predecessor_a = solution.Predecessor(move.node_a);
   Node successor_a = solution.Successor(move.node_a);
   Node predecessor_b = solution.Predecessor(move.node_b);
   Node successor_b = solution.Successor(move.node_b);
+
+  //Change the route by swapping the nodes
   solution.Link(predecessor_a, move.node_b);
   solution.Link(move.node_b, successor_a);
   solution.Link(predecessor_b, move.node_a);
   solution.Link(move.node_a, successor_b);
+
+  // Update route head if the first node was the original head
   if (!predecessor_a)     
     context.SetHead(route_index, move.node_b);
+   // Update route context after the exchange
   context.UpdateRouteContext(solution, route_index, predecessor_a);
 }
 
+// Inner function to calculate delta cost of exchanging two nodes
+// Parameters:
+// - problem: Problem instance with distance matrix
+// - solution: Current solution
+// - node_a: First node to be considered for exchange
+// - node_b: Second node to be considered for exchange
+// - best_move: Reference to store the best exchange move found
+// - best_delta: Reference to track the best delta (cost improvement)
 void ExchangeInner(const Problem &problem, const SpecificSolution &solution, Node node_a, Node node_b,
                     ExchangeMove &best_move, Delta<int> &best_delta) {
   Node predecessor_a = solution.Predecessor(node_a);
   Node successor_a = solution.Successor(node_a);
   Node predecessor_b = solution.Predecessor(node_b);
   Node successor_b = solution.Successor(node_b);
+
+  // Calculate delta cost of exchanging nodes
+  // Compares route distances before and after potential exchange
+
   int delta
       = problem.distance_matrix[solution.Customer(predecessor_a)][solution.Customer(node_b)]
         + problem.distance_matrix[solution.Customer(node_b)][solution.Customer(successor_a)]
@@ -37,27 +61,40 @@ void ExchangeInner(const Problem &problem, const SpecificSolution &solution, Nod
         - problem.distance_matrix[solution.Customer(node_a)][solution.Customer(successor_a)]
         - problem.distance_matrix[solution.Customer(predecessor_b)][solution.Customer(node_b)]
         - problem.distance_matrix[solution.Customer(node_b)][solution.Customer(successor_b)];
+
+  // Update best move if current exchange provides a cost improvement
   if (best_delta.Update(delta)) {
     best_move = {node_a, node_b};
   }
 }
 
+// Operator implementing the exchange move for route optimization
+// Parameters:
+// - problem: Problem instance with routing constraints
+// - route_index: Index of the route being optimized
+// - solution: Current solution to be modified
+// - context: Route context tracking route-specific information
+// Returns: Boolean indicating if an improvement was made
 bool Exchange::operator()(const Problem &problem, Node route_index,
                                           SpecificSolution &solution, RouteContext &context) const {
   ExchangeMove best_move{};
   Delta<int> best_delta{};
+
+  // Iterate through nodes in the route
   Node node_a = context.Head(route_index);
   while (node_a) {
     Node node_b = solution.Successor(node_a);
     if (node_b) {
       node_b = solution.Successor(node_b);
       while (node_b) {
+        // Check potential exchange between nodes
         ExchangeInner(problem, solution, node_a, node_b, best_move, best_delta);
         node_b = solution.Successor(node_b);
       }
     }
     node_a = solution.Successor(node_a);
   }
+  // If an improvement is found, perform the exchange
   if (best_delta.value < 0) {
     DoExchange(best_move, route_index, solution, context);
     return true;
--- src/intra_operator/or_opt.cpp
+++ src/intra_operator/or_opt.cpp
@@ -2,6 +2,7 @@
 #include "../../include/delta.h"
 #include "../../include/route_context.h"
 
+// Structure representing an Or-opt move in route optimization
 struct OrOptMove {
   bool reversed;
   Node head;
@@ -10,50 +11,91 @@ struct OrOptMove {
   Node successor;
 };
 
+// Performs the actual Or-opt move in a given route
+// Parameters:
+// - move: The OrOptMove containing details of the segment to be moved
+// - route_index: Index of the route where move occurs
+// - solution: Current solution being modified
+// - context: Route context tracking route-specific information
 void DoOrOpt(const OrOptMove &move, Node route_index, SpecificSolution &solution, RouteContext &context) {
+  // Find predecessor of head and successor of tail in original route
   Node predecessor_head = solution.Predecessor(move.head);
   Node successor_tail = solution.Successor(move.tail);
+
+  // Temporarily set initial successor to route head
   solution.SetSuccessor(0, context.Head(route_index));
   solution.Link(predecessor_head, successor_tail);
-  if (!move.reversed) {
+
+  // Insert segment based on whether it needs to be reversed
+  if (!move.reversed) {// Normal insertion
     solution.Link(move.predecessor, move.head);
     solution.Link(move.tail, move.successor);
-  } else {
+  } else {// Reversed segment insertion
     solution.ReversedLink(move.head, move.tail, move.predecessor, move.successor);
   }
+  // Update route head
   context.SetHead(route_index, solution.Successor(0));
 }
 
+// Inner function to calculate delta cost of Or-opt move
+// Template parameter num: number of consecutive nodes to move (1, 2 or 3)
+// Parameters:
+// - problem: Problem instance with distance matrix
+// - solution: Current solution
+// - head: Start of segment to move
+// - tail: End of segment to move
+// - predecessor: Potential insertion point predecessor
+// - successor: Potential insertion point successor
+// - best_move: Reference to store the best Or-opt move found
+// - best_delta: Reference to track the best delta (cost improvement)
 template <int num> void OrOptInner(const Problem &problem, const SpecificSolution &solution, Node head,
                                     Node tail, Node predecessor, Node successor,
                                     OrOptMove &best_move, Delta<int> &best_delta) {
+  // Find predecessor of head and successor of tail in original route
   Node predecessor_head = solution.Predecessor(head);
   Node successor_tail = solution.Successor(tail);
+
+  // Calculate base delta by removing the segment
   int delta
       = problem.distance_matrix[solution.Customer(predecessor_head)]
                                 [solution.Customer(successor_tail)]
         - problem.distance_matrix[solution.Customer(predecessor_head)][solution.Customer(head)]
         - problem.distance_matrix[solution.Customer(tail)][solution.Customer(successor_tail)]
         - problem.distance_matrix[solution.Customer(predecessor)][solution.Customer(successor)];
-  bool reversed = false;
+  
+  bool reversed = false; // Flag to track if segment needs to be reversed
+  
+  // Calculate delta for normal insertion
   int insertion_delta
       = problem.distance_matrix[solution.Customer(predecessor)][solution.Customer(head)]
         + problem.distance_matrix[solution.Customer(successor)][solution.Customer(tail)];
+
+  // For moves involving more than one node, check reversed insertion
   if (num > 1) {
     int reversed_delta
         = problem.distance_matrix[solution.Customer(predecessor)][solution.Customer(tail)]
           + problem.distance_matrix[solution.Customer(successor)][solution.Customer(head)];
+    // Update if reversed insertion is more beneficial
     if (reversed_delta < insertion_delta) {
       insertion_delta = reversed_delta;
       reversed = true;
     }
   }
   delta += insertion_delta;
+  // Update best move if current move provides cost improvement
   if (best_delta.Update(delta)) {
     best_move = {reversed, head, tail, predecessor, successor};
   }
 }
 
+// Operator implementing the Or-opt move for route optimization
+// Template parameter num: number of consecutive nodes to move (1, 2, or 3)
+// Parameters:
+// - problem: Problem instance with routing constraints
+// - route_index: Index of the route being optimized
+// - solution: Current solution to be modified
+// - context: Route context tracking route-specific information
+// Returns: Boolean indicating if an improvement was made
 template <int num>
 bool OrOpt<num>::operator()(const Problem &problem, Node route_index,
                                             SpecificSolution &solution, RouteContext &context) const {
@@ -64,8 +106,11 @@ bool OrOpt<num>::operator()(const Problem &problem, Node route_index,
   for (Node i = 0; tail && i < num - 1; ++i) {
     tail = solution.Successor(tail);
   }
+
+  // Iterate through all possible segment moves
   while (tail) {
     Node predecessor, successor;
+    // Check insertions after current tail
     predecessor = solution.Successor(tail);
     while (predecessor) {
       successor = solution.Successor(predecessor);
@@ -73,6 +118,7 @@ bool OrOpt<num>::operator()(const Problem &problem, Node route_index,
                       best_delta);
       predecessor = successor;
     }
+    // Check insertions before current head
     successor = solution.Predecessor(head);
     while (successor) {
       predecessor = solution.Predecessor(successor);
@@ -80,9 +126,12 @@ bool OrOpt<num>::operator()(const Problem &problem, Node route_index,
                       best_delta);
       successor = predecessor;
     }
+    // Move to next segment
     head = solution.Successor(head);
     tail = solution.Successor(tail);
   }
+
+  // If an improvement is found, perform the Or-opt move
   if (best_delta.value < 0) {
     DoOrOpt(best_move, route_index, solution, context);
     context.UpdateRouteContext(solution, route_index, 0);
